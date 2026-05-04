c02b3ec3ca2a0ee1b2520e02cd33cce54f5e484f
Raonhn <nathanraoliveira@hotmail.com>
2026-05-03 23:57:00 -0300

    fix: expose fastify entrypoint for vercel

--- src/infrastructure/server/start-server.ts
+++ src/infrastructure/server/start-server.ts
@@ -1,18 +1,19 @@
 import * as dotenv from 'dotenv'
+import { FastifyInstance } from 'fastify'
 import { mongoConnection } from '../db/mongodb'
 import { buildServer } from './build-server'
 
 dotenv.config()
 
-export async function startServer () {
-  const server = await buildServer()
+export async function startServer (server?: FastifyInstance) {
+  const app = await buildServer(server)
 
   try {
     await mongoConnection.connect()
     mongoConnection.setupGracefulShutdown()
     console.log('MongoDB conectado')
 
-    await server.listen({
+    await app.listen({
       host: '0.0.0.0',
       port: Number(process.env.PORT ?? 3000),
     })
